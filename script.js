70c1f52ca0c247513a2741128b53c4c0626d4e07
thuotracy <tracyjacobs775@gmail.com>
2024-09-05 13:48:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,193 @@
+$(document).ready(function(){
+    $("#about").click(function(){
+        $(".aboutcontent").show()
+        $(".homecontent").hide()
+        $(".theorycontent").hide()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").hide()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "orange"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+    $("#home").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").show()
+        $(".theorycontent").hide()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").hide()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "orange"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+    
+    $("#theory").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").hide()
+        $(".theorycontent").show()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").hide()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "orange"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+    $("#upiliimpact").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").hide()
+        $(".theorycontent").hide()
+        $(".impactcontent").show()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").hide()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "orange"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+    $("#whyupili").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").hide()
+        $(".theorycontent").hide()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").show()
+        $(".teamcontent").hide()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "orange"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+
+    $("#ourteam").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").hide()
+        $(".theorycontent").hide()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").show()
+        $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "orange"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+
+    $("#blog").click(function(){
+        // $(".aboutcontent").hide()
+        // $(".homecontent").hide()
+        // $(".theorycontent").hide()
+        // $(".impactcontent").hide()
+        // $(".whyupilicontent").hide()
+        // $(".teamcontent").show()
+        // $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "orange"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "black"})
+    })
+
+    $("#donate").click(function(){
+        // $(".aboutcontent").hide()
+        // $(".homecontent").hide()
+        // $(".theorycontent").hide()
+        // $(".impactcontent").hide()
+        // $(".whyupilicontent").hide()
+        // $(".teamcontent").show()
+        // $(".contactcontent").hide()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "orange"})
+        $("#contact").css({"color": "black"})
+    })
+
+
+    $("#contact").click(function(){
+        $(".aboutcontent").hide()
+        $(".homecontent").hide()
+        $(".theorycontent").hide()
+        $(".impactcontent").hide()
+        $(".whyupilicontent").hide()
+        $(".teamcontent").hide()
+        $(".contactcontent").show()
+        
+        
+        $("#about").css({"color": "black"})
+        $("#home").css({"color": "black"})
+        $("#theory").css({"color": "black"})
+        $("#upiliimpact").css({"color": "black"})
+        $("#whyupili").css({"color": "black"})
+        $("#ourteam").css({"color": "black"})
+        $("#blog").css({"color": "black"})
+        $("#donate").css({"color": "black"})
+        $("#contact").css({"color": "orange"})
+    })
+})
